25f43209bcf5582402ea4f96fa72a8b2b7b30158
seboX <abckte@gmail.com>
2021-01-17 23:17:53 +0100

    Display login

---
+++ src/components/Guest/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <form @submit.prevent="submit">
+      <input type="text" name="login" id="login" placeholder="login" v-model="form.login"><br />
+      <input type="password" name="password" id="password" placeholder="hasło" v-model="form.password"><br />
+      <button type="submit">Loguj</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+export default {
+  name: 'Login',
+  components: {
+
+  },
+  data() {
+    return {
+      form: {
+        login: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    submit() {
+      const response = axios.post("login");
+      console.log(response);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: null,
+    user: null
   },
   mutations: {
   },
