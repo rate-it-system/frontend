cc10a8c43d86ad46e864128b6e6a68da62a5cde0
seboX <abckte@gmail.com>
2021-01-19 15:45:26 +0100

    display information about reason of faild login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    {{ messages}}
     <router-view />
   </div>
 </template>
+<script>
+import {mapGetters} from 'vuex';
+export default {
+  computed:{
+    ...mapGetters({
+      messages: 'messages'
+    })
+  },
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -11,31 +11,55 @@ export default new Vuex.Store({
         user: null,
         messages: []
     },
+    getters: {
+        authed(state) {
+            return state.token && state.user;
+        },
+        user(state) {
+            return state.user;
+        },
+        messages(state) {
+            return state.messages;
+        },
+    },
     mutations: {
-        SET_TOKEN(state, token){
+        SET_TOKEN(state, token) {
             state.token = token;
         },
-        SET_USER(state, user){
+        SET_USER(state, user) {
             state.user = user;
+        },
+        ADD_MESSAGE(state, message) {
+            state.messages.push(message);
         }
     },
     actions: {
         async login({dispatch}, credentials) {
-
-            const response = await axios.post("login", credentials);
-            axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
-            dispatch('attempt', response.data.token)
+            try {
+                const response = await axios.post("login", credentials);
+                axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
+                dispatch('attempt', response.data.token);
+            } catch (error) {
+                if (error.response) {
+                    dispatch('addMessage', error.response.data.errors);
+                }
+            }
         },
 
-        async attempt({ commit}, token){
+        async attempt({commit}, token) {
             commit('SET_TOKEN', token);
 
             try {
                 const response = await axios.get('user');
                 commit('SET_USER', response.data);
-            } catch (e){
-                console.log("nok");
+            } catch (e) {
+                commit('ADD_MESSAGE', "error");
+                commit('SET_TOKEN', null);
+                commit('SET_USER', null);
             }
+        },
+        async addMessage({commit}, message) {
+            commit('ADD_MESSAGE', message);
         }
     },
     modules: {}
