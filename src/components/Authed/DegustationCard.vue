4f86827865c7797634eda46d7f6ae56743a2615f
seboX <abckte@gmail.com>
2021-01-19 18:02:36 +0100

    move degustation to component

---
+++ src/components/Authed/DegustationCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="DegustatyonCard">
+    <input class="card-header" v-if="degustation.editing" type="text" name="name" v-model="degustation.name">
+    <div v-else class="card-header">{{ degustation.name }}</div>
+    <div class="card-body">
+      {{ degustation.description }}
+      <div class="card">
+        <div class="card-header">Zaproś znajomych</div>
+        <input class="card-body" v-model="degustation.invitation_key" disabled>
+      </div>
+      <a class="btn btn-success float-right" style="margin-left: 5px;">Start</a>
+      <button v-if="degustation.editing" class="btn btn-success float-right" v-on:click="degustation.editing = true" style="margin-left: 5px;">Zapisz</button>
+      <button v-else class="btn btn-info float-right" v-on:click="degustation.editing = true" style="margin-left: 5px;">Edytuj</button>
+      <a class="btn btn-info float-right" style="margin-left: 5px;">Lista uczestników</a>
+      <button v-on:click="deleteDegustation(degustation)" class="btn btn-danger float-right" style="margin-left: 5px;">Usuń</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "DegustatyonCard",
+  props: {
+    degustation: Object
+  },
+};
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,20 +5,8 @@
 
       <div class="row justify-content-center">
         <div class="col-md-8">
-          <div class="card" style="margin-bottom: 5px;" v-for="degustation in degustations" :key="degustation.id">
-            <div class="card-header">{{ degustation.name }}</div>
-            <div class="card-body">
-              {{ degustation.description }}
-              <div class="card">
-                <div class="card-header">Zaproś znajomych</div>
-                <input class="card-body" v-model="degustation.invitation_key" disabled>
-              </div>
-              <a class="btn btn-success float-right" style="margin-left: 5px;">Start</a>
-              <a class="btn btn-info float-right" style="margin-left: 5px;">Edytuj</a>
-              <a class="btn btn-info float-right" style="margin-left: 5px;">Lista uczestników</a>
-              <button v-on:click="deleteDegustation(degustation)" class="btn btn-danger float-right" style="margin-left: 5px;">Usuń</button>
-            </div>
-          </div>
+          <DegustationCard class="card" style="margin-bottom: 5px;" v-for="degustation in degustations" :key="degustation.id" v-bind:degustation="degustation">
+          </DegustationCard>
           <a class="btn btn-info float-right" style="margin-left: 5px;">Następne</a>
           <router-link to="/createDegustation" class="btn btn-success float-right" style="margin-left: 5px;">+Nowa degustacja+</router-link>
           <a class="btn btn-info float-left" style="margin-left: 5px;">Poprzednie</a>
@@ -30,9 +18,13 @@
 
 <script>
 import axios from "axios";
+import DegustationCard from "@/components/Authed/DegustationCard";
 
 export default {
   name: "Dashboard",
+  components: {
+    DegustationCard
+  },
   data() {
     return {
       degustations: []
