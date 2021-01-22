5d9fb0826533f7a0b552003d567d2ff1f2103c87
adrianli12 <57113722+adrianli12@users.noreply.github.com>
2021-01-22 18:18:33 +0100

    route button

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -90,9 +90,9 @@
           </p>
           <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div class="rounded-md shadow">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+              <router-link to="/working"  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                 Jak to działa?
-              </a>
+              </router-link>
             </div>
             <div class="mt-3 sm:mt-0 sm:ml-3">
               <router-link to="/login" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
